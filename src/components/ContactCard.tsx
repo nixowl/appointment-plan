32d6f1bd89cd98d5c4c2e3ab1bea716c098b5f01
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-01-27 17:40:45 +0100

    make names display with first letter capitalized even when input isn't

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -7,13 +7,17 @@ export const ContactCard = ({ givenName, familyName, email, phone }: Contact) =>
     const capsGivenName = givenName[0].toUpperCase() + givenName.slice(1)
     const capsFamilyName = familyName[0].toUpperCase() + familyName.slice(1)
     return (
-        <div className="flex flex-1 items-center space-x-4 bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-black/30 via-neutral-700/30 to-white/10 border-background border-2 p-3 rounded-md h-[9rem]">
+        <div className="flex flex-1 items-center space-x-4 bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-black/30 via-neutral-700/30 to-white/10 border border-white/40 rounded-md p-6 h-[7rem]">
             <div className="bg-primary rounded-full p-6">
-                <p className="text-3xl text-secondary">{firstInitial.toUpperCase()}
-                {surnameInitial.toUpperCase()}</p>
+                <p className="text-3xl text-secondary">
+                    {firstInitial.toUpperCase()}
+                    {surnameInitial.toUpperCase()}
+                </p>
             </div>
             <div className="flex flex-col justify-around p-4 h-full w-full">
-                <p className="text-2xl">{capsGivenName} {capsFamilyName}</p>
+                <p className="text-2xl">
+                    {capsGivenName} {capsFamilyName}
+                </p>
                 <p>{phone}</p>
                 <p>{email}</p>
             </div>
